060953f374b52c40e00d2dd0d0f2764fa3876f6f
Nhan Nguyen <trungnhan21.12@gmail.com>
2021-01-27 17:09:31 +0700

    Update 10.almostIncreasingSequence.py

--- 10.almostIncreasingSequence.py
+++ 10.almostIncreasingSequence.py
@@ -1,5 +1,7 @@
 #Code by Nhan Nguyen
 #Available on github.com/nhannt201
+
+#Cách 1 - Dùng tạm
 def almostIncreasingSequence(arr):
     boolean = False
     #check lan 1
@@ -16,15 +18,6 @@ def almostIncreasingSequence(arr):
                     boolean = True
                 else:
                     boolean = False
-##            for x in range(len(arr)):
-##                if x == 0 and x == 1:
-##                    continue
-##                else:
-##                    if x == len(arr) - 1:
-##                        break
-##                    else:
-##                        if arr[x] > arr[x+1]:
-##                             boolean = False
     else:
         boolean = False
 
@@ -33,4 +26,38 @@ def almostIncreasingSequence(arr):
             
 
 sequence = [1, 3, 2, 1]
+print(almostIncreasingSequence(sequence))
+
+#Cách 2: Ổn - Đã thử nhiều case hơn
+#Theo cách này thì so sánh 2 số liên tiếp    
+
+def almostIncreasingSequence(arr):
+    boolean = False
+    up = 0
+    down = 0
+    for x in range(len(arr)):
+        if x == len(arr) - 1:
+            break
+        else:
+            if arr[x] < arr[x+1] or arr[x] == arr[x+1]:
+                up = up + 1
+            else:
+                down = down + 1
+
+    if up == 1 and down == 1:
+        boolean = True
+    elif up > down and down < 2:
+        if len(arr) > 4:
+            boolean = check3so(arr)
+        else:
+            boolean = True
+        
+    else:
+        boolean = False
+        
+    return boolean #str(up) + " " + str(down)
+    
+            
+
+sequence = [1, 5, 2, 4, 2 ,8, 9, 12, 15]
 print(almostIncreasingSequence(sequence))
