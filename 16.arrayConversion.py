04ac61c9f68fa2e1a0f1687639bc96d3055937ac
Nhan Nguyen <trungnhan21.12@gmail.com>
2021-01-30 17:00:31 +0700

    Update 16.arrayConversion.py
    Thêm code bản T=tham khảo

--- 16.arrayConversion.py
+++ 16.arrayConversion.py
@@ -38,3 +38,39 @@ def arrayConversion(arr):
    
 inputArray = [1, 2, 3, 4, 5, 6, 7, 8]
 print(arrayConversion(inputArray))
+
+#Cái dưới đây là sau khi đã tham khảo 1 code khác, nhìn chung test thấy kết quả không khác lắm
+#Tham khảo: https://gist.github.com/wdospinal/a28ccd0c15a3befa7250a08672992d30
+
+def arrLe(arr):
+    new_arr = []
+    for x in range(len(arr)-1):
+         if x % 2 == 0:
+            new_arr.append(arr[x] + arr[x+1])
+    return new_arr
+
+def arrChan(arr):
+    arr_rs = []
+    for y in range(len(arr)-1):
+        if y % 2 == 0:
+           arr_rs.append(arr[y] * arr[y+1])
+    return arr_rs
+
+def arrayRecursive(inputArray, state):
+    if (len(inputArray)) == 1:
+        return inputArray[0]
+    else:
+        if state:
+            return arrayRecursive(arrLe(inputArray), False)
+        else:
+            return arrayRecursive(arrChan(inputArray), True)
+
+
+
+def arrayConversion(arr):
+    return arrayRecursive(inputArray, True)
+                
+   
+inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(arrayConversion(inputArray))
+
